480fa549bc332f192e3e69f5855cddd085548493
seman <seman@DESKTOP-VCCSGRP>
2023-07-17 15:53:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyat = 15;
+            string aciklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { };
+
+            Urunler urun1= new Urunler(); 
+
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urunler urun2 = new Urunler();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 55;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urunler[] urun = new Urunler[] { urun1, urun2 };
+
+            foreach (var icinde in urun)
+            {
+
+
+                Console.WriteLine(icinde.Adi);  
+                Console.WriteLine(icinde.Fiyati);   
+                Console.WriteLine(icinde.Aciklama);
+                Console.WriteLine("------------------");
+
+
+            }
+            
+            Console.WriteLine("----------------------Metotlar----------------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.ekle(urun1);
+            sepetManager.ekle(urun2);
+             
+            sepetManager.ekle2 ("armut", " Yeşil Armut",12);
+            sepetManager.ekle2("Elma", " Yeşil elma", 12);
+            sepetManager.ekle2("Karpuz", " Diyarbakır karpuzu", 12);
+
+
+        }
+
+
+    }
+}
---
+++ SepetManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+   class SepetManager
+    {  //naming convention 
+    
+          public void ekle(Urunler urun )
+        {
+
+
+            Console.WriteLine("Sepete eklendi." + urun.Adi);
+
+        }
+
+        public void ekle2 (string urunAdi , string aciklama , double fiyat)
+        {
+
+            Console.WriteLine("Sepete eklendi." + urunAdi);
+
+        }
+    }
+}
